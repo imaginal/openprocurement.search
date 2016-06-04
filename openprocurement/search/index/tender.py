2823b1272a46777bd95ccba9156fe3c0c6cf2e17
Volodymyr Flonts <flyonts@gmail.com>
2016-06-04 18:44:13 +0300

    update test_noindex

--- openprocurement/search/index/tender.py
+++ openprocurement/search/index/tender.py
@@ -10,25 +10,26 @@ class TenderIndex(BaseIndex):
     __index_name__ = 'tenders'
 
     def test_noindex(self, item):
-        proc_type = item.data.procurementMethodType
-
-        if proc_type == 'negotiation' or proc_type == 'negotiation.quick':
-            active = 0
-            for award in item.data.get('awards', []):
-                if award.get('status', '') == 'active':
-                    active += 1
-                    break
-            if active == 0:
-                return True
-
-        elif proc_type == 'reporting':
-            active = 0
-            for contract in item.data.get('contracts', []):
-                if contract.get('status', '') == 'active':
-                    active += 1
-                    break
-            if active == 0:
-                return True
+        # don't index by procurementMethodType should working
+        # for tenders created since 2016-05-31
+        if item.data.tenderID > 'UA-2016-05-31':
+            proc_type = item.data.procurementMethodType
+            if proc_type == 'negotiation' or proc_type == 'negotiation.quick':
+                active = 0
+                for award in item.data.get('awards', []):
+                    if award.get('status', '') == 'active':
+                        active += 1
+                        break
+                if active == 0:
+                    return True
+            elif proc_type == 'reporting':
+                active = 0
+                for contract in item.data.get('contracts', []):
+                    if contract.get('status', '') == 'active':
+                        active += 1
+                        break
+                if active == 0:
+                    return True
 
         return False
 
