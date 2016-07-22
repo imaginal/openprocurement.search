58967be5e6dd4d91342668741106b63c70308c7c
Volodymyr Flonts <flyonts@gmail.com>
2016-07-22 20:00:38 +0300

    redefine clean_indexes add --all option

--- openprocurement/search/clean_indexes.py
+++ openprocurement/search/clean_indexes.py
@@ -8,7 +8,7 @@ import urllib2
 import simplejson as json
 from ConfigParser import ConfigParser
 
-FORMAT='%(asctime)-15s %(levelname)s %(processName)s %(message)s'
+FORMAT='%(asctime)-15s %(levelname)s %(message)s'
 logger=logging.getLogger(__name__)
 
 
@@ -24,41 +24,41 @@ def delete_index(elastic_host, name):
         logger.error("%d %s", resp.getcode(), data)
 
 
-def process_index(elastic_host, index_list, prefix, current):
-    logger.info("Process index prefix %s", prefix)
-
+def process_all(elastic_host, index_list, index_yaml):
     fresh_time = time.time() - (10*24*3600) # 10 days
     candidates = list()
-    skip_count = 0
 
-    for index in index_list:
+    current_keys  = index_yaml.keys()
+    current_names = index_yaml.values()
+
+    for index in sorted(index_list):
         name = index['index']
-        if not name.startswith(prefix+'_'):
-            logger.debug("Skip by prefix %s", name)
+        prefix, created = name.rsplit('_', 1)
+        if prefix not in current_keys:
+            logger.debug("Skip prefix %s", name)
             continue
-        if name == current:
+        if name in current_names:
             logger.info("Skip current %s", name)
-            skip_count += 1
             continue
-        name_prefix, created_time = name.rsplit('_', 1)
-        if int(created_time) > fresh_time:
+        if int(created) > fresh_time:
             logger.info("Skip fresh %s", name)
-            skip_count += 1
             continue
-        if name.startswith(prefix):
-            candidates.append((name, created_time))
+        candidates.append(name)
 
     if len(candidates) < 1:
         logger.info("Not enought candidates")
         return
 
-    if skip_count < 2:
-        candidates = sorted(candidates, key=lambda i: i[1])
-        name, created = candidates.pop()
-        logger.info("Skip youngest %s", name)
-
-    for name,created in candidates:
+    for name in candidates:
         delete_index(elastic_host, name)
+
+
+def delete_all(elastic_host, index_list, prefix):
+    logger.warning("Delete all by prefix %s", prefix)
+    for index in index_list:
+        name = index['index']
+        if name.startswith(prefix+'_'):
+            delete_index(elastic_host, name)
 
 
 def get_indexes(elastic_host):
@@ -79,10 +79,14 @@ def process_config(config):
 
     yafile = parser.get('search_engine', 'index_names')
     with open("%s.yaml" % yafile) as f:
-        index_names = yaml.load(f)
+        index_yaml = yaml.load(f)
 
-    for prefix in index_names:
-        process_index(elhost, index_list, prefix, index_names[prefix])
+    if len(sys.argv) > 2 and '--all' in sys.argv:
+        for prefix in index_yaml.keys():
+            delete_all(elhost, index_list, prefix)
+        return
+
+    process_all(elhost, index_list, index_yaml)
 
 
 def main():
@@ -93,6 +97,8 @@ def main():
     logging.basicConfig(level=logging.INFO, format=FORMAT)
 
     for config in sys.argv[1:]:
+        if config.startswith('--'):
+            continue
         process_config(config)
 
 
