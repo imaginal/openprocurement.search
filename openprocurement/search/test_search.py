570beecb0a3bab89915360090cab2957eabb175d
Volodymyr Flonts <flyonts@gmail.com>
2016-11-11 01:19:50 +0200

    add ignore errors mode in test_search

--- openprocurement/search/test_search.py
+++ openprocurement/search/test_search.py
@@ -17,196 +17,260 @@ from openprocurement.search.source.plan import PlanSource
 from openprocurement.search.index.plan import PlanIndex
 
 LOG_FORMAT = '%(asctime)s %(levelname)s %(message)s'
-TEST_LIMIT = 5
 
 logger = logging.getLogger()
-class search:
-    config = {}
-    offset = 15
 
 
-def do_search(query, tender):
-    path = query % tender
-    url = 'http://%s:%s/%s' % (
-        search.config['host'],
-        search.config['port'], path)
-    req = urllib2.urlopen(url, timeout=10)
-    code = req.getcode()
-    resp = req.read()
-    if code != 200:
-        raise RuntimeError("%s - HTTP Error %d" % (path, code))
-    data = json.loads(resp)
-    if 'items' not in data:
-        raise RuntimeError("%s %s - BAD Resp" % (path, tender.id))
-    item = {'id': None}
-    for item in data['items']:
-        if item['id'] == tender['id']:
-            break
-    if item['id'] != tender['id']:
-        raise RuntimeError("%s %s - NOT FOUND api=%s" % (path, tender.id,
-            tender['dateModified']))
-    if item['dateModified'] != tender['dateModified']:
-        raise RuntimeError("%s %s - dateModified mismatch, search=%s api=%s" % (
-            path, tender.id, item['dateModified'], tender['dateModified']))
-    logger.info("Search %s found %s", path, item['dateModified'])
+class SearchTester(object):
+    engine_config = {}
+    search_config = {}
+    full_test = False
+    offset = 30
+    ignore = False
+    errors = 0
 
+    def __init__(self, engine_config, search_config):
+        self.engine_config = dict(engine_config)
+        self.search_config = dict(search_config)
 
-def test_tenders(engine, config, full_test=False):
-    config['skip_until'] = ''
-    source = TenderSource(config)
-    index = TenderIndex(engine, source, config)
+        if self.search_config['host'] == '0.0.0.0':
+            self.search_config['host'] = '127.0.0.1'
 
-    offset = datetime.now() - timedelta(minutes=search.offset)
-    offset = offset.isoformat()
+        self.engine = IndexEngine(self.engine_config)
 
-    if full_test:
-        test_limit = 5000
-        skip_limit = 100
-        preload = 500000
-        limit = 1000
-    else:
-        test_limit = 5
-        skip_limit = 0
-        preload = 100
-        limit = 100
-
-    source.reset()
-    source.client.params.update({'descending': 1, 'limit': limit})
-    source.config['preload'] = preload
-
-    mode = source.client.params.get('mode', None)
-    if mode:
-        logger.info("TendersClient mode %s", mode)
-
-    test_count = 0
-    skip_count = skip_limit
-
-    while test_count < test_limit:
-        meta = None
-        for meta in source.items():
-            if meta.dateModified > offset:
-                continue
-            if skip_count < skip_limit:
-                skip_count += 1
-                continue
-            tender = source.get(meta)
-            if index.test_noindex(tender):
-                continue
-            if tender.data.dateModified > offset:
-                continue
-            do_search('tenders?tid=%(tenderID)s', tender.data)
-            test_count += 1
-            skip_count = 0
-            if test_count >= test_limit:
+    def do_search(self, query, tender):
+        path = query % tender
+        url = 'http://%s:%s/%s' % (
+            self.search_config['host'],
+            self.search_config['port'], path)
+        req = urllib2.urlopen(url, timeout=10)
+        code = req.getcode()
+        resp = req.read()
+        if code != 200:
+            raise RuntimeError("%s - HTTP %d" % (path, code))
+        data = json.loads(resp)
+        if 'items' not in data:
+            raise RuntimeError("%s %s - BAD RESP" % (path, tender.id))
+        item = {'id': None}
+        for item in data['items']:
+            if item['id'] == tender['id']:
                 break
-        if not meta:
-            break
+        if item['id'] != tender['id']:
+            raise RuntimeError("%s %s %s - NOT FOUND" % (path, tender.id,
+                tender['dateModified']))
+        if item['dateModified'] != tender['dateModified']:
+            raise RuntimeError("%s %s search=%s api=%s - dateModified mismatch" % (
+                path, tender.id, item['dateModified'], tender['dateModified']))
+        logger.info("Search %s found %s", path, item['dateModified'])
 
+    def test_tenders(self):
+        # self.engine_config['skip_until'] = ''
 
-def test_plans(engine, config, full_test=False):
-    config['plan_skip_until'] = ''
-    source = PlanSource(config)
-    index = PlanIndex(engine, source, config)
+        source = TenderSource(self.engine_config)
+        index = TenderIndex(self.engine, source, self.engine_config)
 
-    offset = datetime.now() - timedelta(minutes=search.offset)
-    offset = offset.isoformat()
+        offset = datetime.now() - timedelta(minutes=self.offset)
+        offset = offset.isoformat()
 
-    if full_test:
-        test_limit = 5000
-        skip_limit = 100
-        preload = 500000
-        limit = 1000
-    else:
-        test_limit = 5
-        skip_limit = 0
-        preload = 100
-        limit = 100
+        if self.full_test:
+            test_limit = 5000
+            skip_limit = 100
+            preload = 500000
+            limit = 1000
+        else:
+            test_limit = 5
+            skip_limit = 0
+            preload = 100
+            limit = 100
 
-    source.reset()
-    source.client.params.update({'descending': 1, 'limit': limit})
-    source.config['plan_preload'] = preload
-    source.skip_until = None
+        source.reset()
+        source.client.params.update({'descending': 1, 'limit': limit})
+        source.config['preload'] = preload
 
-    mode = source.client.params.get('mode', None)
-    if mode:
-        logger.info("PlansClient mode %s", mode)
+        logger.info("Client %s/api/%s mode=%s", 
+            source.config['api_url'], 
+            source.config['api_version'],
+            source.client.params.get('mode', ''))
+        logger.info("Search %s:%s", 
+            self.search_config['host'],
+            self.search_config['port'])
 
-    test_count = 0
-    skip_count = skip_limit
+        test_count = 0
+        skip_count = skip_limit
 
-    while test_count < test_limit:
-        meta = None
-        for meta in source.items():
-            if meta.dateModified > offset:
-                continue
-            if skip_count < skip_limit:
-                skip_count += 1
-                continue
-            tender = source.get(meta)
-            if index.test_noindex(tender):
-                continue
-            if tender.data.dateModified > offset:
-                continue
-            do_search('plans?pid=%(planID)s', tender.data)
-            test_count += 1
-            skip_count = 0
-            if test_count >= test_limit:
+        while test_count < test_limit:
+            meta = None
+            for meta in source.items():
+                if meta.dateModified > offset:
+                    continue
+                if skip_count < skip_limit:
+                    skip_count += 1
+                    continue
+                try:
+                    tender = source.get(meta)
+                    if index.test_noindex(tender):
+                        continue
+                    if tender.data.dateModified > offset:
+                        continue
+                    self.do_search('tenders?tid=%(tenderID)s', tender.data)
+                except Exception as e:
+                    if self.ignore:
+                        logger.error("%s (ignored)", e)
+                        self.errors += 1
+                    else:
+                        raise
+                test_count += 1
+                skip_count = 0
+                if test_count >= test_limit:
+                    break
+            if not meta:
                 break
-        if not meta:
-            break
+
+        if test_count < 5:
+            raise RuntimeError("Not enough queries")
+
+
+    def test_plans(self):
+        # self.engine_config['plan_skip_until'] = ''
+
+        source = PlanSource(self.engine_config)
+        index = PlanIndex(self.engine, source, self.engine_config)
+
+        offset = datetime.now() - timedelta(minutes=self.offset)
+        offset = offset.isoformat()
+
+        if self.full_test:
+            test_limit = 5000
+            skip_limit = 100
+            preload = 500000
+            limit = 1000
+        else:
+            test_limit = 5
+            skip_limit = 0
+            preload = 100
+            limit = 100
+
+        source.reset()
+        source.client.params.update({'descending': 1, 'limit': limit})
+        source.config['plan_preload'] = preload
+        source.skip_until = None
+
+        logger.info("Client %s/api/%s mode=%s", 
+            source.config['plan_api_url'], 
+            source.config['plan_api_version'],
+            source.client.params.get('mode', ''))
+        logger.info("Search %s:%s", 
+            self.search_config['host'],
+            self.search_config['port'])
+
+        test_count = 0
+        skip_count = skip_limit
+
+        while test_count < test_limit:
+            meta = None
+            for meta in source.items():
+                if meta.dateModified > offset:
+                    continue
+                if skip_count < skip_limit:
+                    skip_count += 1
+                    continue
+                try:
+                    tender = source.get(meta)
+                    if index.test_noindex(tender):
+                        continue
+                    if tender.data.dateModified > offset:
+                        continue
+                    self.do_search('plans?pid=%(planID)s', tender.data)
+                except Exception as e:
+                    if self.ignore:
+                        logger.error("%s (ignored)", e)
+                        self.errors += 1
+                    else:
+                        raise
+                test_count += 1
+                skip_count = 0
+                if test_count >= test_limit:
+                    break
+            if not meta:
+                break
+
+        if test_count < 5:
+            raise RuntimeError("Not enough queries")
+
+
+    def test(self):
+        if self.engine_config.get('api_url', None):
+            self.test_tenders()
+
+        if self.engine_config.get('plan_api_url', None):
+            self.test_plans()        
+
+
+def print_usage():
+    print("Usage: test_search etc/search.ini [options]")
+    print("Options are:")
+    print("\t-d\t- enable debug mode")
+    print("\t-f\t- search 1% of all")
+    print("\t-i\t- ignore errors")
+    print("\t-nt\t- don't test tenders")
+    print("\t-np\t- don't test plans")
+    print("\t-q\t- be quiet")
 
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: test_search etc/search.ini [options]")
-        print("Options are:")
-        print("\t-d\t- enable debug mode")
-        print("\t-f\t- search 1% of all")
-        print("\t-q\t- be quiet")
+        print_usage()
         sys.exit(1)
 
+    offset = None
     parser = ConfigParser()
     for arg in sys.argv[1:]:
-        if arg[:9] == '--offset=':
-            search.offset = int(arg[9:])
+        if arg.startswith('--offset='):
+            _, offset = arg.split('=', 1)
             continue
-        if arg[:1] == '-':
+        if arg.startswith('-'):
             continue
         parser.read(arg)
 
-    config = dict(parser.items('search_engine'))
-    search.config = dict(parser.items('server:main'))
-    if search.config['host'] == '0.0.0.0':
-        search.config['host'] = '127.0.0.1'
-
-    engine = IndexEngine(config)
+    tester = SearchTester(
+        parser.items('search_engine'),
+        parser.items('server:main'))
+    if offset:
+        tester.offset = int(offset)
 
     log_level = logging.INFO
     if '-d' in sys.argv:
         log_level = logging.DEBUG
     if '-q' in sys.argv:
         log_level = logging.ERROR
-    full_test = False
     if '-f' in sys.argv:
-        full_test = True
+        tester.full_test = True
+    if '-i' in sys.argv:
+        tester.ignore = True
+    if '-nt' in sys.argv:
+        tester.engine_config['api_url'] = None
+    if '-np' in sys.argv:
+        tester.engine_config['plan_api_url'] = None
 
     logging.basicConfig(level=log_level, format=LOG_FORMAT)
     logger.info("Start with config %s", sys.argv[1])
 
     try:
-        if config.get('api_url', None):
-            test_tenders(engine, config, full_test)
-
-        if config.get('plan_api_url', None):
-            test_plans(engine, config, full_test)
+        tester.test()
     except KeyboardInterrupt:
+        logger.info("User interrupt")
         pass
     except Exception as e:
         if '-d' in sys.argv:
-            raise
+            logger.exception("%s", e)
         logger.error("%s", e)
         return 1
 
+    if tester.errors:
+        logger.warning("Total %d error(s)", tester.errors)
+        return 2
+
+    logger.info("Test passed.")
     return 0
 
 
