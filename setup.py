69c56eaf480122f40e73588c90b0dd74b30f7164
Volodymyr Flonts <flyonts@gmail.com>
2016-11-12 03:42:26 +0200

    update package setup.py to 0.6

--- setup.py
+++ setup.py
@@ -1,55 +1,48 @@
 from setuptools import setup, find_packages
 
-
-version = '0.1'
-
+version = '0.6a1'
 
 setup(name='openprocurement.search',
         version=version,
-        description="OpenProcurement search service and indexer",
+        description="OpenProcurement search service",
         long_description=open("README.md").read(),
         # Get more strings from
         # http://pypi.python.org/pypi?:action=list_classifiers
         classifiers=[
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python",
+            "License :: OSI Approved :: Apache Software License",
+            "Programming Language :: Python",
         ],
-        keywords='OpenProcurement',
-        author='E-Democracy NGO',
-        author_email='info@ed.org.ua',
+        keywords='openprocurement procurement prozorro search',
+        author='Volodymyr Flonts',
+        author_email='flyonts@gmail.com',
         license='Apache License 2.0',
-        url='https://github.com/imaginal/openprocurement.search',
+        url='https://github.com/openprocurement/openprocurement.search',
         packages=find_packages(),
         namespace_packages=['openprocurement'],
         include_package_data=True,
         zip_safe=False,
         install_requires=[
-          'setuptools',
-          'requests',
-          'retrying',
-          'iso8601',
-          'python-dateutil',
-          'simplejson',
-          'Flask',
-          'gevent',
-          'PyYAML',
-          # 'sse',
-          # 'request_id_middleware',
-          # ssl warning
-          'openprocurement_client',
-          'elasticsearch==1.9.0',
+            'elasticsearch==1.9.0',
+            'openprocurement_client',
+            'Flask',
+            'iso8601',
+            'PyYAML',
+            'requests',
+            'retrying',
+            'simplejson',
         ],
         entry_points={
-          'console_scripts': [
-              'index_worker = openprocurement.search.index_worker:main',
-              'search_server = openprocurement.search.search_server:main',
-              'clean_indexes = openprocurement.search.clean_indexes:main',
-              'ocds_ftp_sync = openprocurement.search.ocds_ftp_sync:main',
-              'test_search = openprocurement.search.test_search:main',
-              'update_orgs = openprocurement.search.update_orgs:main',
-          ],
-          'paste.app_factory': [
-              'search_server = openprocurement.search.search_server:make_app'
-          ]
+            'console_scripts': [
+                'index_worker = openprocurement.search.index_worker:main',
+                'search_server = openprocurement.search.search_server:main',
+                'clean_indexes = openprocurement.search.clean_indexes:main',
+                'ocds_ftp_sync = openprocurement.search.ocds_ftp_sync:main',
+                'test_search = openprocurement.search.test_search:main',
+                'update_orgs = openprocurement.search.update_orgs:main',
+            ],
+            'paste.app_factory': [
+                'search_server = openprocurement.search.search_server:make_app'
+            ]
         }
-        )
+    )
+
