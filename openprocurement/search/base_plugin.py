ed618448828d5781794a873c7ca6c6ece83041c5
Volodymyr Flyonts <flyonts@gmail.com>
2018-08-31 13:03:11 +0300

    update api of base_plugin

--- openprocurement/search/base_plugin.py
+++ openprocurement/search/base_plugin.py
@@ -3,13 +3,19 @@
 
 class BasePlugin(object):
     __name__ = __name__
+    index_mappings = {}
     search_maps = {}
+
+    # Plugin api
+    start_in_subprocess = None
+    before_create_index = None
+    before_source_reset = None
+    before_source_items = None
+    before_process_index = None
+    before_index_item = None
 
     def __init__(self, config):
         pass
 
     def __repr__(self):
         return "<%s:%s>" % (self.__name__, self.__class__.__name__)
-
-    def before_index_item(self, index, item):
-        pass
