c42c6a6858c7c91b1d972890c5ed338d06ec3cc8
Volodymyr Flyonts <flyonts@gmail.com>
2018-11-10 14:46:50 +0200

    v0.9 beta 4 plugin 2

--- openprocurement/search/version.py
+++ openprocurement/search/version.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-__version_info__ = (0, 9, '0beta3' + '+plugin2.0')
+__version_info__ = (0, 9, '0b4' + '+p2')
 __version__ = '.'.join((str(entry) for entry in __version_info__))
